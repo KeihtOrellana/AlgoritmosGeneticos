c689c850927e75845e8d5b4aa6a7d798116167b4
keihtorelllana <korellanac@ing.ucsc.cl>
2025-08-19 22:49:10 -0400

    generar numeros real y entero randomico

---
+++ utilidades.py
@@ -0,0 +1,10 @@
+import random
+
+#Generar un número real randóminco entre [0 y 1].
+def real_randomico():
+    return random.random()
+
+#Generar un número entero randóminco entre [0 y 1].
+def entero_randomico(N):
+    return random.randint(0,N)
+
